4a95f0bb52a9a84c5a7bef6b817e140dfd20ea89
julesbardos <36551160+julesbadros@users.noreply.github.com>
2018-02-26 16:45:49 -0500

    Robot C folder

---
+++ Robotc/Robotmovment.c
@@ -0,0 +1,13 @@
+task main{
+  //All the robot code
+  setMotor(leftMotor,60);
+  setMotor(rightMotor,60);
+  wait(3,seconds);
+  setMotor(leftMotor,-60);
+  setMotor(rightMotor,-60);
+  wait(3,seconds);
+  //Make the robot spin around
+  setMotor(leftMotor,127);
+  setMotor(rightMotor,0);
+  wait(1,seconds);
+}
